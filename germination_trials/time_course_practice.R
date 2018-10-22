1d028e2f3d5311367e91a20bb5e1269a1a3bc7a0
dbuona <dbuonaiuto@g.harvard.edu>
2018-10-22 15:32:57 -0400

    observations

--- germination_trials/time_course_practice.R
+++ germination_trials/time_course_practice.R
@@ -16,8 +16,8 @@ data6<-read.csv("time_course_practice6.csv",header=TRUE)
 
 data<-dplyr::select(data,-c(X))
 data3<-dplyr::select(data3,-c(X,X.1))
-data5<-dplyr::select (data5,1:13)
-data6<-dplyr::select(data6,1:10)
+data5<-dplyr::select (data5,1:14)
+data6<-dplyr::select(data6,1:11)
 
 colnames(data)<-(c("zero_day","tot_seed","Taxa","INC","COLD","plate_num","8/28/18","8/29/18",	"8/30/18",	"8/31/18",	"9/3/18",	"9/5/18",	"9/7/18",	"9/9/18",	"9/12/18",	"9/14/18",	"9/16/18",	"9/17/18","9/18/18","9/20/18","9/21/18"))
 ncol(data)
@@ -28,12 +28,12 @@ colnames(data2)<-(c("zero_day","tot_seed","Taxa","INC","COLD","plate_num", "9/12
 data3$"9/24/18"<-0
 colnames(data3)<-(c("zero_day","tot_seed","Taxa","INC","COLD","plate_num","9/26/18","9/28/18","9/30/18","10/03/18","10/05/18","10/07/18","10/08/18","10/10/18","10/12/18","10/14/18","10/15/18","10/17/18","10/19/18","9/24/18"))
 
-colnames(data4)<-(c("zero_day","tot_seed","Taxa","INC","COLD","plate_num","10/01/18","10/03/18","10/05/18","10/07/18","10/08/18","10/10/18","10/12/18","10/14/18","10/15/18","10/17/18","10/19/18","10/21/18"))
+colnames(data4)<-(c("zero_day","tot_seed","Taxa","INC","COLD","plate_num","10/01/18","10/03/18","10/05/18","10/07/18","10/08/18","10/10/18","10/12/18","10/14/18","10/15/18","10/17/18","10/19/18","10/21/18","10/22/18"))
 
 data5$"10/08/18"<-0
-colnames(data5)<-(c("zero_day","tot_seed","Taxa","INC","COLD","plate_num","10/10/18","10/12/18","10/14/18","10/15/18","10/17/18","10/19/18","10/21/18","10/08/18"))
+colnames(data5)<-(c("zero_day","tot_seed","Taxa","INC","COLD","plate_num","10/10/18","10/12/18","10/14/18","10/15/18","10/17/18","10/19/18","10/21/18","10/22/18","10/08/18"))
 
-colnames(data6)<-(c("zero_day","tot_seed","Taxa","INC","COLD","plate_num","10/15/18","10/17/18","10/19/18","10/21/18"))
+colnames(data6)<-(c("zero_day","tot_seed","Taxa","INC","COLD","plate_num","10/15/18","10/17/18","10/19/18","10/21/18","10/22/18"))
 
 data<-gather(data,"date","germination",7:21)
 data$germination<-as.numeric(data$germination)
@@ -44,13 +44,13 @@ data2$germination<-as.numeric(data2$germination)
 data3<-gather(data3,"date","germination",7:20)
 data3$germination<-as.numeric(data3$germination)
 
-data4<-gather(data4,"date","germination",7:18)
+data4<-gather(data4,"date","germination",7:19)
 data4$germination<-as.numeric(data4$germination)
 
-data5<-gather(data5,"date","germination",7:14)
+data5<-gather(data5,"date","germination",7:15)
 data5$germination<-as.numeric(data5$germination)
 
-data6<-gather(data6,"date","germination",7:10)
+data6<-gather(data6,"date","germination",7:11)
 data6$germination<-as.numeric(data6$germination)
 
 

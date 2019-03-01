3690fd9255e73fd91c37b743cdfa838274414bfc
dbuona <dbuonaiuto@g.harvard.edu>
2019-03-01 14:22:06 -0500

    got fake data going

--- germination_trials/nonlinearmodel/rewind_fake.R
+++ germination_trials/nonlinearmodel/rewind_fake.R
@@ -1,27 +1,89 @@
+##Began by Dan and Lizzie late Feb 2019
+###updated most recently by Dan on March 1 2019
+##Purpose is to simulat germiantion data a kind to Dan's trial
+
+##next step, a thoughtful way to add error between replicates
+#1. I could add error on each parementer, I think using rnorm
+# or just add error to the final why value
 rm(list=ls())
 options(stringsAsFactors = FALSE)
 graphics.off()
 
-time<-seq(0,24,by=3)
-treat<-c(0,1)
+setwd("~/Documents/git/timetogerminate/germination_trials")
+
+library(rstan)
+library(tidyr)
+library(drc)
+library(dplyr)
+library(shinystan)
+time<-seq(0,24,by=3) #time of each trial
+treat<-c(0,14,28,35,42,49,56,63,77,91) # level of chilling, continuous data
+force<-c(0,1)# 2 levels of forching, low/high
 
 ##parameters
-t50.a<-15
-t50.b<--6
-beta.b<--4
-
-d.a<-6
-d.b<-10
+t50.a<-20 #intercept of t50
+t50.b<--0.09 # slope of t50 with chilling
+t50.f<--1 #slope of t50 with forcing
 
 
+beta.a<--4 #intercept of beta (shape paramenter)
+beta.b<--0.05 # slope of beta with chilling
+beta.f<--1.5 # slope of beta with forcing
+
+d.a<-0 # intercept of d (maximum germination)
+d.b<-0.19 #slope of d with chilling
+d.f<-2 # slope of d with forcing
+
+repz<-seq(1,10,by=1) ## number of replicates
 
 
-for (i in c(1:length(treat))){
-for (l in time){
-  y<-(d.b*treat[i]+d.a)/(1+((time/(t50.a)^beta.b)))
-dfhere<-data.frame(time=time, y=y,treat=treat)
+# Trying running just this:  ##DB, rthis doesn't run, but I think it was just trouble shooting
+#l <- 1
+#  y<-(d.b*treat[i]+d.a)/(1+((time/(t50.a)^beta.b)))
+#  dfhere <- data.frame(time=time, y=y,treat=rep(treat[i], length(y)))
+#  df <- rbind(df, dfhere)
+# This tells you that in each loop of l you get ALL the time runs ... so this means the way your time loop works it does the above NINE TIMES
+# 9 * 9 * 2 treats = 162
+# So, now we know the time loop is the problem.... and actually it make me think that you just don't need it ...
+
+
+##This is the base loop 
+df<-data.frame(time=numeric(), y=numeric(),chilltreat=numeric(),forcetreat=numeric(),ID=numeric())  ##3why is this breaking?
+
+  for (i in c(1:length(treat))){
+   y <- c()
+  for (l in c(1:length(force))){
+    y<-c()
+    for(k in c(1:length(repz))){ 
+      d.error<-rnorm(1,0,0.5)
+      t50.error<-rnorm(1,0,0.5)
+      beta.error<-rnorm(1,0,0.5)
+      y<-(d.b*treat[i]+d.f*force[l]+d.a+d.error)/(1+((time/(t50.b*treat[i]+t50.f*force[l]+t50.a+t50.error))^(beta.b*treat[i]+beta.f*force[l]+beta.a+beta.error)))
+  dfhere <- data.frame(time=time, y=y,chilltreat=rep(treat[i], length(y)),forcetreat=rep(force[l],length(y)),ID=rep(repz[k],length(y))) ## make a data frame for each level, this over rights so
+  df <- rbind(df, dfhere) ## rbind it here for safty
 }
 }
+}
 
-dfhere$y<-round(dfhere$y)
+##df$y<-round(df$y) #maybe I don't want this if I am trying to get the model to return the error
+ ##plot the data
+ploty<-ggplot(df,aes(time,y))+geom_point(aes(color=as.factor(chilltreat),shape=as.factor(forcetreat)))
+ploty
+ploty+geom_line(stat = "summary", fun.y = mean, aes(color=as.factor(chilltreat),linetype=as.factor(forcetreat))) ### plot point with mean lines
 
+chillonly<-filter(df,forcetreat==0)### try to model just chilling for now.
+
+###try it in a stan model
+data.list.chill <- with(chillonly, 
+                   list(Y=y, 
+                        t = time,
+                        chill=chilltreat,
+                        N = nrow(chillonly)
+                   )
+)
+
+
+germ.mod.chillonly = stan('stan/fakeseed_wchill_emw.stan', data = data.list.chill,
+                 iter = 3000, warmup=2000) 
+summary(germ.mod.chillonly) # 700+ divergent transitions. 
+launch_shinystan(germ.mod.chillonly)
---
+++ germination_trials/stan/fakeseed_wchill_emw.stan
@@ -0,0 +1,60 @@
+data {
+  int<lower=1> N;
+
+  real t[N]; //this is time 
+  real Y[N]; //this is the response
+  vector[N] chill; // this is the amount of chilling
+} 
+
+parameters {
+
+  real b_chill_beta; //effect of chilling on sliope of S curve
+  real b_chill_t50; //effect of chilling on t50.
+  real b_chill_d; //effect of chill on max germination
+  
+  real a_beta;  // alpha for beta
+  real a_t50; //alpha for t50
+  real a_d;  //alpha for max germination
+  real<lower=0>  sigma; //sigma
+  
+
+} 
+
+transformed parameters {
+real y_hat[N];
+   for (i in 1:N)
+y_hat[i] =(b_chill_d*chill[i]+a_d)/(1+(((t[i])/(b_chill_t50*chill[i]+a_t50))^(b_chill_beta*chill[i]+a_beta))); //log logistic equation where paramenters beta, t50 and d are represent as chilling sub mod
+     
+     }
+ 
+
+ model {
+  // priors
+  //t50 ~ uniform(0, 100); 
+  //beta ~ normal(0, 50); 
+  //d ~ uniform(0, 30); 
+  a_beta~normal(0,10);
+  a_t50~normal(0,10);
+  a_d ~ normal(0,10);
+  b_chill_beta ~normal(0,1);
+  b_chill_t50 ~normal(0,1);
+  b_chill_d ~normal(0,1);
+  
+  sigma ~ normal(0, 10);
+ 
+ 
+  // likelihood
+  Y ~ normal(y_hat, sigma);
+}
+
+//generated quantities{
+  //real Y_mean[N]; 
+ //real Y_pred[N];
+  //for(i in 1:N){
+    // Posterior parameter distribution of the mean
+  //  Y_mean[i] = d/(1+((t[i]/t50)^beta));
+    // Posterior predictive distribution
+  // Y_pred[i] = normal_rng(Y_mean[i], sigma);   
+//}
+//}
+

d5de5213358a341de19eb3858434557e71b52848
dbuona <dbuonaiuto@g.harvard.edu>
2019-03-26 16:02:42 -0400

    tinkered with an unseccuessful survival model

--- germination_trials/survival_analysis/surival_analysis.R
+++ germination_trials/survival_analysis/surival_analysis.R
@@ -12,24 +12,21 @@ library("icenReg")
 library("emmeans")
 library(drc)
 library(lme4)
+library(brms)
 setwd("~/Documents/git/timetogerminate/germination_trials/input")
 
-d<-read.csv("surival_dat_nointerval.csv")
+d<-read.csv("..//survival_analysis/surival_dat_nointerval.csv")
+d$DAY<-ifelse(d$DAY==0,0.001,d$DAY)
+d$
+as<-filter(d,Taxa=="Asclepias syriaca")
+priorz<-get_prior(DAY | cens(germinated) ~ INC + chill_time,data=as,family = weibull())
 
-
+fit2 <- brm(DAY | cens(germinated) ~ INC + chill_time, 
+            data=as, family = weibull(), inits = "0",prior=priorz) ##doesnt run
+summary(fit2)
 ###nonlinear mixed effect model: 
 #1ignore those that didn't germinate
-?survreg.distributions
-germy<-filter(d,germinated==1)
 
-mod <- survreg(Surv(DAY,germinated) ~ INC+COLD+frailty(Taxa), dist = "gaussian", data = d) 
-summary(mod)
-
-emmeans(mod,~COLD+INC,transform="response")
-
-mod2 <- survreg(Surv(DAY,germinated) ~ INC+COLD, dist = "gaussian", data = germy) 
-summary(mod2)
-emmeans(mod2,~COLD+INC,transform="response")
 
 ggplot(germy,aes(DAY))+geom_histogram(bins=25)+facet_wrap(~Taxa)
 
